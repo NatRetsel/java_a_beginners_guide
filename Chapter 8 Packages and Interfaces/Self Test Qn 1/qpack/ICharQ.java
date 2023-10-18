6a5110297974ac310e7f044b720202736739234a
Lester Tan <127372303+SunsetStories@users.noreply.github.com>
2023-10-18 22:10:44 +0800

    Projects and solutions for chapter 8

---
+++ Chapter 8 Packages and Interfaces/Self Test Qn 1/qpack/ICharQ.java
@@ -0,0 +1,10 @@
+package qpack;
+
+// A character queue interface.
+public interface ICharQ {
+    //put a character into the queue.
+    void put(char ch);
+
+    // Get a character from the queue
+    char get();
+}
